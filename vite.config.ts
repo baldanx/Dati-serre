bf867398693ffc3087dd5060804319bdae3e6951
baldanx <bldnx@msn.com>
2026-05-15 14:08:13 +0200

    feat: Add PWA assets and update config
    Integrates PWA functionality by adding necessary assets and updating the Vite configuration to correctly reference them. This includes generating various icon sizes and updating the `manifest.json` configuration in `vite.config.ts`. Also updates `index.html` to link to the new favicon and apple touch icon.

--- vite.config.ts
+++ vite.config.ts
@@ -22,9 +22,24 @@ export default defineConfig(({mode}) => {
           display: 'standalone',
           icons: [
             {
-              src: 'icon.svg',
-              sizes: '192x192 512x512',
-              type: 'image/svg+xml',
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
               purpose: 'any maskable'
             }
           ]
